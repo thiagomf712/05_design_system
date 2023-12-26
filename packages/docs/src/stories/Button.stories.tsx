ebcc934f72e1f00cd78a082ecdb3f86600598972
Thiago Ferreira <thiagomf712@gmail.com>
2023-12-26 19:39:48 -0300

    Learning - Day 2

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+    variant: 'primary',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,39 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  padding: '$4',
-})
-
-export function App() {
-  return <Button>Hello World</Button>
+export type ButtonProps = {
+  size: 'small' | 'big'
+  variant: 'primary' | 'secondary' | 'tertiary'
 }
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $8',
+      },
+    },
+    variant: {
+      primary: {
+        backgroundColor: '$ignite500',
+      },
+      secondary: {
+        backgroundColor: '$ignite300',
+      },
+      tertiary: {
+        backgroundColor: '$ignite100',
+      },
+    },
+  },
+})
